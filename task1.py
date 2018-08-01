258c831b74ddf0ea2a85fbc4d6f5b1651f461e3c
Nyakio Muriuki <warugurujackline1@gmail.com>
2018-08-01 09:12:09 +0300

    insert commit

--- task1.py
+++ task1.py
@@ -1,9 +1,10 @@
-import random 
+import random
 
 ourList = list()
-count = 0 
+count = 0
 while (count < 11):
     ourList.append(random.randint(1,10))
     count += 1
-    
-ourList
+
+belowFive = [num for num in ourList if num <= 5]
+print(belowFive)
--- task2.py
+++ task2.py
@@ -3,4 +3,5 @@ import random
 words = [word.rstrip('\n') for word in open('words.txt')]
 randomPhrase = " ".join([words[random.randrange(0, len(words))] for i in range(4)])
 
-randomPhrase
+reversePhrase = randomPhrase[::-1]
+print(reversePhrase)
